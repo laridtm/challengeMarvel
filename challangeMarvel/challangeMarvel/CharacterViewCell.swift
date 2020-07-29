be4d1802a05d6cc97dd76c1606fabf14ad0dea0a
Larissa Diniz <larissa.dtm@gmail.com>
2020-07-28 22:11:41 -0300

    created collectionView

---
+++ challangeMarvel/challangeMarvel/CharacterViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  CharacterViewCell.swift
+//  challangeMarvel
+//
+//  Created by Larissa Diniz on 28/07/20.
+//  Copyright © 2020 Larissa Diniz. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterName: UILabel!
+    
+    let httpClient = HTTPClient()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layoutIfNeeded()
+        layer.cornerRadius = 8
+    }
+    
+    func setCharacterImage(url: String) {
+        if let apiUrl = URL(string: url) {
+            httpClient.get(url: apiUrl) { result -> Void in
+                switch result {
+                case .failure:
+                    break
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let loadedImage = UIImage(data: data)
+                        self.characterImage.image = loadedImage
+                    }
+                    break
+                }
+            }
+        }
+    }
+    
+    func setCharacterName(name: String) {
+        characterName.text = name
+    }
+}
